0f4f77f85cd195e689b930de3891bc703eb5e8c4
Diego Torres Pérez <87044529+DiegoTorresP@users.noreply.github.com>
2023-01-22 00:28:55 -0600

    Agregar archivos de proyecto.

---
+++ Data/BookNexusContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookNexus.Models;
+
+namespace BookNexus.Data
+{
+    public class BookNexusContext : DbContext
+    {
+        public BookNexusContext (DbContextOptions<BookNexusContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookNexus.Models.Book> Book { get; set; } = default!;
+    }
+}
